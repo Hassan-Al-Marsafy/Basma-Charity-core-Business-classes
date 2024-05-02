a4e9d9604d2ebcfb59d610a42e5b9bdad1924e57
Hassan <hassanelmarsafy@gmail.com>
2024-05-02 10:57:39 +0300

    added Guest class

---
+++ Guest.php
@@ -0,0 +1,49 @@
+<?php
+require_once 'User.php';
+require_once 'ManagingClass.php';
+class Guest extends User {
+    private $manager;
+    private $pdo;
+    function __construct($id, $name, $pdo, $manager) {
+        parent::__construct($id, $name, 'guest', $pdo);
+        $this->manager = $manager;
+        $this->manager->insertUser($this);
+        $this->pdo = $pdo;
+    }
+
+    function signUp($password) {
+        // Change user type to 'RegisteredUser'
+        $this->setType('RegisteredUser');
+        $this->manager->updateUser($this, $this->getId(), $this->getName(), $this->getType());
+
+        // Add user id and password to login_info table
+        $sql = "INSERT INTO login_info (id, password) VALUES (?, ?)";
+        $stmt= $this->pdo->prepare($sql);
+        return $stmt->execute([$this->getId(), $password]);
+    }
+
+    function makeDonation($id, $date, $userId, $accountantId, $managerId){
+        $this->manager->createDonation($id, $date, $userId, $accountantId, $managerId);
+        $this->manager->insertDonation();
+    }
+
+    function readDonations() {
+        $donations = $this->manager->readAllUserDonations($this);
+        foreach ($donations as $donation) {
+            echo "Donation id: {$donation->getId()}, date: {$donation->getDate()}, user id: {$donation->getUserId()}, accountant id: {$donation->getAccountantId()}, manager id: {$donation->getManagerId()}\n";
+        }
+    }
+
+    function readDonationDetails() {
+        $donations = $this->manager->readAllUserDonations($this);
+        foreach ($donations as $donation) {
+            $donationDetails = $this->manager->readAllDonationsDetails($donation);
+            foreach ($donationDetails as $detail) {
+                echo "DonationDetails id: {$detail->getId()}, donation id: {$detail->getDonationId()}, donation type id: {$detail->getDonationTypeId()}, quantity: {$detail->getQuantity()}\n";
+            }
+        }
+    }
+
+    // You can add more methods here...
+}
+?>
