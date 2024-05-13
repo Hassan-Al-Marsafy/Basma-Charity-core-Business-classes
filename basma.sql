186b9489f08af0e81f29f9e5906159c50f609c65
Aley Amin <aley.amin.22@gmail.com>
2024-05-13 23:44:09 +0300

    Added MySQ file

---
+++ basma.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 13, 2024 at 10:34 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `basma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donations`
+--
+
+CREATE TABLE `donations` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `accountant_id` int(11) NOT NULL,
+  `manager_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donation_details`
+--
+
+CREATE TABLE `donation_details` (
+  `id` int(20) NOT NULL,
+  `donation_id` int(20) NOT NULL,
+  `donationType_id` int(10) NOT NULL,
+  `quantity` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donation_types`
+--
+
+CREATE TABLE `donation_types` (
+  `id` int(10) NOT NULL,
+  `D_type_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `donation_types`
+--
+
+INSERT INTO `donation_types` (`id`, `D_type_name`) VALUES
+(10, 'money'),
+(11, 'money');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_info`
+--
+
+CREATE TABLE `login_info` (
+  `id` int(10) NOT NULL,
+  `password` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login_info`
+--
+
+INSERT INTO `login_info` (`id`, `password`) VALUES
+(23, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `user_type`) VALUES
+(23, 'John Doe', 'RegisteredUser');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `donations`
+--
+ALTER TABLE `donations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `u_id_d` (`user_id`);
+
+--
+-- Indexes for table `donation_details`
+--
+ALTER TABLE `donation_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dt_id_dd` (`donationType_id`),
+  ADD KEY `d_id_dd` (`donation_id`);
+
+--
+-- Indexes for table `donation_types`
+--
+ALTER TABLE `donation_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_info`
+--
+ALTER TABLE `login_info`
+  ADD KEY `login_id` (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `donations`
+--
+ALTER TABLE `donations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `donation_details`
+--
+ALTER TABLE `donation_details`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `donation_types`
+--
+ALTER TABLE `donation_types`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `donations`
+--
+ALTER TABLE `donations`
+  ADD CONSTRAINT `u_id_d` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `donation_details`
+--
+ALTER TABLE `donation_details`
+  ADD CONSTRAINT `dt_id_dd` FOREIGN KEY (`donationType_id`) REFERENCES `donation_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `login_info`
+--
+ALTER TABLE `login_info`
+  ADD CONSTRAINT `login_id` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
