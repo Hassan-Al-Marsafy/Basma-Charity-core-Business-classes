ce6a533b70ccface2e86a2ed69d20b6a515f0046
abdalrahman-alkawwa <abdalrahmanalkawwa15@gmail.com>
2024-05-15 17:53:17 +0300

    Read donation details control and view

---
+++ View/ReadDonationDetailsView.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Read Donation Details</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper {
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Put an ID to Read Donation Details</h2>
+                    </div>
+                    <p>Please fill this form and submit to read donation details from the database.</p>
+                    <form action="../Controller/ReadDonationDetailsController.php" method="GET">
+                        <div class="form-group">
+                            <label>Donation Details ID</label>
+                            <input type="text" name="id" class="form-control" maxlength="50" required>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="../View/OutputReadDonationDetailsView.php" class="btn btn-default">Cancel</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
